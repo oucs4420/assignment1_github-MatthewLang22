f0147b949e3d0972d6624e61bee00e1354753e46
Matthew Lang <gl397316@ohio.edu>
2022-01-12 18:00:48 -0500

    Added code

--- filesize.cc
+++ filesize.cc
@@ -1,18 +1,48 @@
+/*
+* Name:         Matthew Lang
+* Date:         1-14-2022
+* Description:  CS 4420 Assignment 0 - 
+*/
+
 #include <iostream>
 #include <fstream>
+
 using namespace std;
 
 // output format to use IDENTICALLY:
-//BSH:Saru> ./filesize input.3lines input.1line 
-//program: ./filesize
+// BSH:Saru> ./filesize input.3lines input.1line 
+// program: ./filesize
 // input.3lines: 3
 // input.1line: 1
 
-int main( int argc, char* argv[] )
+int main(int argc, char* argv[])
 {
-    // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    ifstream instream;
+    for (int i = 1; i < argc; i++)
+    {
+        instream.open(argv[i]);
+        if (instream.fail())
+        {
+            cout << argv[i] << ": -1" << endl;
+        }
+        else
+        {
+            int line_count = 0;
+            string line;
+            while (!instream.eof())
+            {
+                getline(instream, line);
+                line_count++;
+            }
+            if (line == "")
+            {
+                line_count = line_count - 1;
+            }
+            cout << argv[i] << ": " << line_count << endl;
+            line_count = 0;
+        }
+        instream.close();
+    }
 
     exit(0); // this means that the program executed correctly!
 }
