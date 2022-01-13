a8a6c8cca5ac69662d5536dfef75cde0bbc538d3
Matthew Lang <gl397316@ohio.edu>
2022-01-12 19:17:30 -0500

    Finished

--- filesize.cc
+++ filesize.cc
@@ -1,7 +1,7 @@
 /*
 * Name:         Matthew Lang
 * Date:         1-14-2022
-* Description:  CS 4420 Assignment 0 - 
+* Description:  CS 4420 - Assignment 0 - Github Familiarity: Counts the number of lines in each file
 */
 
 #include <iostream>
@@ -17,28 +17,31 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+    cout << "program: " << argv[0] << endl; // First printing the name of the program
+
     ifstream instream;
-    for (int i = 1; i < argc; i++)
+
+    for (int i = 1; i < argc; i++) // Looping through each file provided
     {
         instream.open(argv[i]);
-        if (instream.fail())
+        if (instream.fail()) // If there is no file with the provided name, print -1
         {
-            cout << argv[i] << ": -1" << endl;
+            cout << " " << argv[i] << ": -1" << endl;
         }
         else
         {
             int line_count = 0;
             string line;
-            while (!instream.eof())
+            while (!instream.eof()) // While loop to count each getline
             {
                 getline(instream, line);
                 line_count++;
             }
-            if (line == "")
+            if (line == "") // If the last line is blank, it needs subtracted out from the count
             {
                 line_count = line_count - 1;
             }
-            cout << argv[i] << ": " << line_count << endl;
+            cout << " " << argv[i] << ": " << line_count << endl;
             line_count = 0;
         }
         instream.close();
